45894617f3b4eebd5a43c00451a1d18f044ba595
dangvanduc90 <ducdangvan@vccorp.vn>
2018-12-07 21:14:52 +0700

    update migration

--- database/migrations/2018_12_07_130728_create_product_attribute_table.php
+++ database/migrations/2018_12_07_130728_create_product_attribute_table.php
@@ -14,6 +14,7 @@ class CreateProductAttributeTable extends Migration
     public function up()
     {
         Schema::create('product_attribute', function (Blueprint $table) {
+            $table->increments('product_attribute');
             $table->integer('product_id')->unsigned();
             $table->foreign('product_id')->references('product_id')->on('products');
             $table->integer('attribute_value_id')->unsigned();
